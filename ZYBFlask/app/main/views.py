d64529ea89adb57f90015881ded360d5c71bb905
awd <awd@163.com>
2019-10-06 21:24:25 +0800

    请假功能完成

--- ZYBFlask/app/main/views.py
+++ ZYBFlask/app/main/views.py
@@ -88,3 +88,94 @@ def register():
         user.save()
 
     return render_template("register.html",**locals())
+
+
+#分页器配置
+class Pager():
+    """
+    分页器需要具备的功能
+        页码
+        分页数据
+        是否第一页
+        是否最后一页
+    """
+    def __init__(self,total_data,page_size):
+        """
+        :param total_data: 要进行分页的总数据
+        :param page_size: 每页多少条数据
+        """
+        self.total_data=total_data
+        self.page_size=page_size
+        self.is_start=False
+        self.is_end = False
+        self.page_count=len(total_data)   #总数据的长度
+        self.next_page=0 #下一页
+        self.previous_page=0 #上一页
+        self.page_number=self.page_count/page_size    #分了多少页
+        #判断是否整除，有余数需要在加一页
+        if self.page_number == int(self.page_number):
+            self.page_number=int(self.page_number)
+        else:
+            self.page_number=int(self.page_number)+1
+        self.page_range=range(1,self.page_number+1)   #最后得到的页码范围
+    def page_data(self,page):
+        """
+        返回的分页数据
+        :param page: 页码
+        思路：
+        page_size=10
+        1  start 0  end 10
+        2  start 10 end 20
+        3  start 20  end 30
+        """
+        self.next_page=int(page)+1
+        self.previous_page=int(page)-1
+        if page <=self.page_range[-1]:   #如果页面小等于页码范围最大的数
+            page_start = (page -1)*self.page_size
+            page_end=page*self.page_size
+            data=self.total_data[page_start:page_end]
+            if page == 1:
+                self.is_start = True
+            else:
+                self.is_start = False
+            if page == self.page_range[-1]:
+                self.is_end = True
+            else:
+                self.is_end = False
+        else:
+            data=["没有数据"]
+        return data
+
+@main.route("/holiday_leave/", methods=["GET", "POST"])
+@loginValid
+# @csrf.exempt
+def holiday_leave():
+    if request.method == "POST":
+        username = request.form.get("request_name")
+        type = request.form.get("request_type")
+        start_time = request.form.get("request_start_time")
+        end_time = request.form.get("request_end_time")
+        phone = request.form.get("request_phone")
+        description = request.form.get("request_description")
+
+        leave = Leave()
+        leave.request_id = request.cookies.get("id")
+        leave.request_name = username
+        leave.request_type = type
+        leave.request_description = description
+        leave.request_start_time = start_time
+        leave.request_end_time = end_time
+        leave.request_phone = phone
+        leave.request_status = "0"
+        leave.save()
+        return redirect("/leave_list/1/")
+    return render_template("holiday_leave.html")
+
+
+@main.route("/leave_list/<int:page>/")
+@loginValid
+def leave_list(page):
+    leaves = Leave.query.all()
+    pager = Pager(leaves, 2)  # 实例化一个分页器leaves为总数据，3为每页的数据条数
+    page_data = pager.page_data(page)  # 对应页码所显示的数据
+    return render_template("leave_list.html", **locals())
--- ZYBFlask/app/models.py
+++ ZYBFlask/app/models.py
@@ -17,3 +17,24 @@ class User(BaseModel):
     user_name=models.Column(models.String(32))
     password=models.Column(models.String(32))
     email=models.Column(models.String(32))
+
+class Leave(BaseModel):
+    """
+    请假人id
+    请假人姓名
+    假期类型
+    假期起始时间
+    假期结束时间
+    请假描述
+    联系方式
+    请假状态：请假  批准  驳回   销假
+    """
+    __tablename__ = "leave"
+    request_id = models.Column(models.Integer)
+    request_name = models.Column(models.String(32))
+    request_type = models.Column(models.String(32))
+    request_description = models.Column(models.Text)
+    request_start_time = models.Column(models.String(32))
+    request_end_time = models.Column(models.String(32))
+    request_phone = models.Column(models.String(32))
+    request_status = models.Column(models.String(32))
